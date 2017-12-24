9b0366436013e16829fbeba97edad2cc5fc4e8f0
sbernheim <sambernheim@gmail.com>
2017-12-23 23:43:22 -0500

    Add pie chart tp budget component

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import '../scss/budget.scss'
+import '../scss/budget.scss';
+
+import Chart from 'chart.js';
+
 class Budget extends Component {
     constructor(props){
         super(props);
@@ -8,6 +11,7 @@ class Budget extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.updateChart = this.generateChart.bind(this);
     }
 
     handleChange(event) {
@@ -18,37 +22,75 @@ class Budget extends Component {
         const graph = document.querySelector('.budget--graph > div');
         let percentage = (this.props.totalSpent / event.target.value) * 100;
         graph.style.width = percentage + "%";
+
+        const spent = this.props.totalSpent;
+        let remaining = (event.target.value - this.props.totalSpent).toFixed(2);
+        if (remaining <= 0) {
+            remaining = 0;
+        }
+
+        // Update the pie chart
+        this.generateChart(spent, remaining);
     }
     
     numberWithCommas(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
+    generateChart(totalSpent, totalRemaining) {
+        console.log("totalSpent: ", totalSpent);
+        console.log("totalRemaining: ", totalRemaining);
+
+        const ctx = document.querySelector('#myChart');
+
+        let myChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ["Spent", "Remaining"],
+                datasets: [{
+                    backgroundColor: ["rgb(212,99,99)", "rgb(77, 153, 114)"],
+                    data: [
+                        totalSpent,
+                        totalRemaining
+                    ]
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "This Month's Budget"
+                }
+            }
+        });
+    }
+
     render() {
 
         let spent = this.numberWithCommas(this.props.totalSpent);
 
-        let left = (this.state.monthlyBudget - this.props.totalSpent).toFixed(2);
-        left = this.numberWithCommas(left);
+        let remaining = (this.state.monthlyBudget - this.props.totalSpent).toFixed(2);
+        remaining = this.numberWithCommas(remaining);
 
         return (
             <div className='budget'>
-                
+
                 <h2 className='budget--total-spent'>Spent: ${spent}</h2>
-                <h2 className='budget--total-left'>Left: ${left}</h2>
+                <h2 className='budget--total-left'>Remaining: ${remaining}</h2>
 
                 <form className='budget--form'>
                     <label>
                         <span>Monthly Budget:</span>
-                        <input placeholder='Enter your budget' type='number' name='budget' value={this.state.value} onChange={this.handleChange} />
+                        <input placeholder='Enter your budget' type='number' name='budget' value={this.state.monthlyBudget} onChange={this.handleChange} />
                     </label>
                 </form>
 
                 <p className='budget--percent'>{((this.props.totalSpent / this.state.monthlyBudget)*100).toFixed(2) || 0}% spent</p>
+
                 <div className='budget--graph'>
                     <div></div>
                 </div>
                 
+                <canvas className='budget--pie-char' id="myChart"></canvas>
 
             </div>
         );
