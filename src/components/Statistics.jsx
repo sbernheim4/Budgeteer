760727c8aed29ac1729c6d5f36edd2cc33c88f62
sbernheim <sambernheim@gmail.com>
2018-01-03 11:57:52 -0500

    Only show relevent categories for doughnutchart in statistics component

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -4,7 +4,7 @@ import { Doughnut, Line, Bar } from 'react-chartjs-2';
 import '../scss/statistics.scss';
 
 class Statistics extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			categoryDoughnutData: {},
@@ -14,15 +14,20 @@ class Statistics extends Component {
 
 	componentDidMount() {
 		this.generateDoughnutChart();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 		this.generateLineChart();
 	}
 
+
 	calculateDoughnutAmounts() {
 		// Initialize a new array of size 8 and fill it with 0s initially
-		let amts = new Array(8);
+		let amts = new Array(14);
 		amts.fill(0);
 
-		this.props.transactions.forEach( t => {
+		this.props.transactions.forEach(t => {
 
 			let category = t.category || [''];
 			let amount = t.amount;
@@ -49,13 +54,30 @@ class Statistics extends Component {
 				case 'Healthcare':
 					amts[6] += amount;
 					break;
-				default:
+				case 'Bank Fees':
 					amts[7] += amount;
 					break;
+				case 'Cash Advance':
+					amts[8] += amount;
+					break;
+				case 'Interest':
+					amts[9] += amount;
+					break;
+				case 'Payment':
+					amts[10] += amount;
+					break;
+				case 'Tax':
+					amts[11] += amount;
+					break;
+				case 'Transfer':
+					amts[12] += amount;
+					break;
+				default:
+					amts[13] += amount
 			}
 		});
 
-		amts = amts.map( val => {
+		amts = amts.map(val => {
 			return (Math.round(val * 100) / 100).toFixed(2);
 		})
 
@@ -64,18 +86,10 @@ class Statistics extends Component {
 
 	generateDoughnutChart() {
 		let amounts = this.calculateDoughnutAmounts();
+		let doughnutLabels = this.generateDoughnutLabels(amounts);
 
 		const data = {
-			labels: [
-				'Food and Drink',
-				'Travel',
-				'Shops',
-				'Recreation',
-				'Service',
-				'Community',
-				'Healthcare',
-				'Other'
-			],
+			labels: doughnutLabels,
 			datasets: [{
 				data: amounts,
 				backgroundColor: [
@@ -86,7 +100,13 @@ class Statistics extends Component {
 					'#95DEE3',
 					'#CE3175',
 					'#5A7247',
-					'#CFB095'
+					'#CFB095',
+					'#578CA9',
+					'#f4d942',
+					'#afc47d',
+					'#558244',
+					'#347759',
+					'#2d7582'
 				]
 			}],
 			options: {
@@ -94,10 +114,51 @@ class Statistics extends Component {
 			}
 		};
 
-		this.setState({ categoryDoughnutData: data} );
+		this.setState({ categoryDoughnutData: data });
 	}
 
+	generateDoughnutLabels(amountsArray) {
+
+		let defaultLabelsArray = [
+			'Food and Drink',
+			'Travel',
+			'Shops',
+			'Recreation',
+			'Service',
+			'Community',
+			'Healthcare',
+			'Bank Fees',
+			'Cash Advance',
+			'Interest',
+			'Payment',
+			'Tax',
+			'Transfer',
+			'Other'
+		];
+
+		let labelsArray = [];
+		for (let i = 0; i < amountsArray.length; i++) {
+			if (amountsArray[i] !== "0.00") {
+				labelsArray.push(defaultLabelsArray[i]);
+			}
+		}
+
+		console.log('labels array: ');
+		console.log(labelsArray);
+		console.log(labelsArray.length);
+
+		return labelsArray;
+	}
+
+<<<<<<< Updated upstream
 	generateLineChart() {
+=======
+
+
+
+
+	async generateLineChart() {
+>>>>>>> Stashed changes
 
 		// Get the past year's worth of data
 
@@ -105,15 +166,72 @@ class Statistics extends Component {
 		// Dynamically create labels array
 		// Dynamically create data array
 
+<<<<<<< Updated upstream
+=======
+		this.setState({ lineChartData: data });
+	}
+
+	async getYearlyData() {
+
+		/* Get transactions for the past 365 days */
+		$.post('/plaid-api/transactions', { days: 365 }, data => {
+			if (!data.transactions) {
+				console.error('-----------------------------');
+				throw Error('Invalid data from server');
+			}
+
+			let allTransactions = data.transactions;
+
+			/* Sort the transactions by date */
+			allTransactions = allTransactions.sort((a, b) => {
+				return a.date - b.date;
+			});
+
+			/* Sum up costs by week */
+			let amounts = new Array(52);
+			amounts.fill(0);
+
+			let mostRecentDate = allTransactions[0].date;
+			let year = mostRecentDate.slice(0, 4);
+			let month = mostRecentDate.slice(5, 7);
+			let day = mostRecentDate.slice(8);
+
+			// Most recent transaction's date
+			let x = new Date(year, month, day);
+			let i = 0;
+
+			allTransactions.forEach(t => {
+				let transactionDate = new Date(t.date.slice(0, 4), t.date.slice(5, 7), t.date.slice(8));
+
+				if (isWithinRange(transactionDate, startOfWeek(x), endOfWeek(x))) {
+					amounts[i] += t.amount;
+				} else {
+					i++;
+					// I've moved beyond the current range
+
+					// Go back one week
+					x = subWeeks(x, 1);
+
+					amounts[i] += t.amount;
+				}
+			});
+
+			this.setState({ lineChartBlob: amounts });
+
+			return amounts;
+>>>>>>> Stashed changes
 
 		// Sort the transactions based on date
 		let sortedTransactions = this.props.transactions.sort( (a, b) => {
 			return a.date - b.date;
 		});
 
+<<<<<<< Updated upstream
 		sortedTransactions.reverse();
 	}
 
+=======
+>>>>>>> Stashed changes
 	render() {
 		return (
 
