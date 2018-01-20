2a6f592f594e6fc9817e67166a17b596128e034c
sbernheim <sambernheim@gmail.com>
2018-01-20 11:24:27 -0500

    Add connection to mLab

--- server/index.js
+++ server/index.js
@@ -5,15 +5,26 @@ const app = express();
 const path = require("path");
 const chalk = require("chalk");
 const compression = require('compression');
+const mongoose = require('mongoose');
+const startDb = require('./db');
 
+
+/****************** DB Options ******************/
+const mongodbUri = process.env.DB_URI;
+
+mongoose.connect(mongodbUri);
+let db = mongoose.connection;
+
+/****************** Server Options ******************/
 const port = process.env.PORT;
 const cacheTime = 31536000000;
 
 app.use(compression());
 
-/******************  SERVE STATIC FILES --> JS, CSS, IMAGES ETC ******************/
+/****************** SERVE STATIC FILES --> JS, CSS, IMAGES ETC ******************/
 app.use(express.static(path.join(__dirname, "../public"), { maxAge: cacheTime } ));
 
+/****************** Handle Requests ******************/
 app.use("/plaid-api", require("./plaid-api.js"));
 
 app.all("*", (req, res, next) => {
@@ -26,6 +37,15 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.listen(port, () => {
-	console.log(chalk.green(`Listening on port ${port}`));
-});
+/****************** Start the DB and Server ******************/
+startDb.then( () => {
+    app.listen(port, () => {
+        console.log(chalk.blue(`Listening on port ${port}`));
+    });
+}).catch(err => {
+    console.log(err);
+})
+
+
+
+
