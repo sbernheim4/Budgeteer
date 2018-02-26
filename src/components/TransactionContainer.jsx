d5fa496ee2fabc55469b20e90bc12b3e2749b6d4
sbernheim4 <sambernheim@gmail.com>
2018-02-26 16:50:03 -0500

    Begin work on Show more functionality for transaction container

--- src/components/TransactionContainer.jsx
+++ src/components/TransactionContainer.jsx
@@ -5,17 +5,47 @@ import "../scss/transactionContainer.scss"
 
 class TransactionContainer extends Component {
 	constructor(props) {
-		super(props)
-		this.state = {};
-	}
+        super(props)
+
+        let initialNum = this.props.transactions.length < 10 ? this.props.transactions.length : 10
+
+        this.state = {
+            num: initialNum,
+            transactionsToDisplay: this.props.transactions.slice(0, initialNum)
+        };
+
+        this.showMoreItems = this.showMoreItems.bind(this);
+    }
+
+    showMoreItems() {
+
+        if (this.state.num + 10 > this.props.transactions.length) {
+            this.setState({
+                transactionsToDisplay: this.props.transactions,
+                num: this.props.transactions.length
+            })
+
+            // If all transactions are shown, remove the button
+            document.querySelector("button").remove();
+        } else {
+            this.setState({
+                transactionsToDisplay: this.props.transactions.slice(0, this.state.num + 10),
+                num: this.state.num + 10
+            });
+        }
+
+    }
 
 	render() {
 		return (
 			<div className="transaction-container">
-				{this.props.transactions.map( (t, index) =>
-					<Transaction key={index} transaction={t} />
-				)}
+                {this.state.transactionsToDisplay.map( (t, index) => {
+                    return <Transaction key={index} transaction={t} />
+                })}
+
+                <button onClick={this.showMoreItems}>Show More</button>
 			</div>
+
 		);
 	}
 }
