60dfb0c19aced71c49a299aa514b5507ac30e07e
sbernheim4 <sambernheim@gmail.com>
2018-03-15 16:52:18 -0400

    Basic mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,91 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 
+/* Font Awesome */
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/fontawesome-free-solid';
+
 import "../scss/navbar.scss";
 
 class Navbar extends Component {
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            handler: {}
-        }
+		this.state = {
+			handler: {}
+		}
 
-        this.addAccount = this.addAccount.bind(this);
-    }
+		this.addAccount = this.addAccount.bind(this);
+	}
 
-    async componentDidMount() {
+	async componentDidMount() {
 
-        let keyAndEnv = await fetch('plaid-api/key-and-env');
-        keyAndEnv = await keyAndEnv.json();
+		let keyAndEnv = await fetch('plaid-api/key-and-env');
+		keyAndEnv = await keyAndEnv.json();
 
-        const plaid = Plaid.create({
-            apiVersion: 'v2',
-            clientName: 'Plaid Walkthrough Demo',
-            env: keyAndEnv.env,
-            product: ['transactions'],
-            key: keyAndEnv.publicKey,
-            onSuccess: function (public_token) {
-                fetch('/plaid-api/get-access-token', {
-                    method: 'post',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        public_token: public_token,
-                        client_id: '5a24ca6a4e95b836d37e37fe',
-                        secret: 'f07a761a591de3cbbc5ac3ba2f4301'
-                    })
-                });
-            }
-        });
+		const plaid = Plaid.create({
+			apiVersion: 'v2',
+			clientName: 'Plaid Walkthrough Demo',
+			env: keyAndEnv.env,
+			product: ['transactions'],
+			key: keyAndEnv.publicKey,
+			onSuccess: function (public_token) {
+				fetch('/plaid-api/get-access-token', {
+					method: 'post',
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						public_token: public_token,
+						client_id: '5a24ca6a4e95b836d37e37fe',
+						secret: 'f07a761a591de3cbbc5ac3ba2f4301'
+					})
+				});
+			}
+		});
 
-        this.setState({ handler: plaid });
-    }
+		this.setState({ handler: plaid });
+	}
 
-    addAccount() {
-        this.state.handler.open();
-    }
+	addAccount() {
+		this.state.handler.open();
+	}
 
-    render() {
+	toggleMenu() {
+		document.querySelector(".navbar--mobile--links").classList.toggle("navbar--mobile--links__active");
+	}
+
+	render() {
 		return (
 			<nav className='navbar'>
-				<ul>
-					<li><Link to='/'>Home</Link></li>
-					<li><Link to='/accounts'>Accounts</Link></li>
-					<li><Link to='/statistics'>Statistics</Link></li>
-					<li><Link to='/networth'>Networth</Link></li>
-				</ul>
+				<div className='navbar--desktop'>
+					<ul>
+						<li><Link to='/'>Home</Link></li>
+						<li><Link to='/accounts'>Accounts</Link></li>
+						<li><Link to='/statistics'>Statistics</Link></li>
+						<li><Link to='/networth'>Networth</Link></li>
+					</ul>
 
-                <div>
-                    <button onClick={this.addAccount}>Add Accounts</button>
-                </div>
+					<div>
+						<button onClick={this.addAccount}>Add Accounts</button>
+					</div>
+				</div>
+
+				<div className="navbar--mobile">
+					<div>
+						<FontAwesomeIcon className="icon" icon={faBars} onClick={this.toggleMenu}/>
+					</div>
+
+					<div className='navbar--mobile--links'>
+						<p><Link onClick={this.toggleMenu} to='/'>Home</Link></p>
+						<p><Link onClick={this.toggleMenu} to='/accounts'>Accounts</Link></p>
+						<p><Link onClick={this.toggleMenu} to='/statistics'>Statistics</Link></p>
+						<p><Link onClick={this.toggleMenu} to='/networth'>Networth</Link></p>
+						<button onClick={this.addAccount}>Add Accounts</button>
+					</div>
+				</div>
 			</nav>
 		);
 	}
