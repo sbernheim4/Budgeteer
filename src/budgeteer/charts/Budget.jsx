76c4473ae90cc447ec3ae4eb53d018c54bf949c9
sbernheim4 <sambernheim@gmail.com>
2018-04-28 15:55:56 -0400

    Add dollar signs to tooltip budget

--- src/budgeteer/charts/Budget.jsx
+++ src/budgeteer/charts/Budget.jsx
@@ -24,8 +24,8 @@ class CustomTooltip extends Component {
 			return (
 
 				<div className="budget--tooltip">
-					<p>Spent: {this.props.spent}</p>
-					<p>Remaining: {this.props.remaining}</p>
+					<p>Spent: ${this.props.spent}</p>
+					<p>Remaining: ${this.props.remaining}</p>
 				</div>
 			);
 		}
