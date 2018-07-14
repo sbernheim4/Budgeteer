7d60f0e84578155f0f75c689910797027e67e205
sbernheim4 <sambernheim@gmail.com>
2018-07-14 12:18:25 -0400

    Separate google maps into its own component

--- src/budgeteer/Statistics/HeatMap/HeatMap.jsx
+++ src/budgeteer/Statistics/HeatMap/HeatMap.jsx
@@ -1,25 +1,17 @@
 /* eslint no-undefined: 0 */
 import React, { Component } from "react";
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import GoogleMap from "./GoogleMap/GoogleMap.jsx";
 
 import "./heatmap.scss";
 
-const MyMapComponent = withGoogleMap((props) =>
-	<GoogleMap
-		defaultZoom={8}
-		defaultCenter={{ lat: -34.397, lng: 150.644 }}
-	>
-		{props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-	</GoogleMap>
-);
 
 class HeatMap extends Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			map: {},
-			heatmap: {} 
+			heatmap: {}
 		}
 	}
 
@@ -31,15 +23,7 @@ class HeatMap extends Component {
 		return (
 			<div className="heatmap">
 
-				<div id="floating-panel">
-					{/*<button onClick={this.changeGradient()}>Change gradient</button>*/}
-					{/*<button onClick={this.changeRadius()}>Change radius</button>*/}
-				</div>
-
-				<div id="map"></div>
-
-					<MyMapComponent isMarkerShown />// Map with a Marker
-					<MyMapComponent isMarkerShown={false} />// Just only Map
+				<GoogleMap />
 
 			</div>
 		)
