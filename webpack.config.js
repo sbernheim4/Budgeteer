b2dc645f8e8d079f15f617bbcfb7f45318dbdc2f
sbernheim4 <sambernheim@gmail.com>
2019-09-29 14:41:24 -0400

    Split node packages into separate chunks

--- webpack.config.js
+++ webpack.config.js
@@ -159,7 +159,27 @@ const clientConfig = {
 		new WebpackBar(),
 
 		new webpack.DefinePlugin(envKeys)
-	]
+    ],
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			maxInitialRequests: Infinity,
+			minSize: 0,
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name(module) {
+						// get the name. E.g. node_modules/packageName/not/this/part.js
+						// or node_modules/packageName
+						const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+						// npm package names are URL-safe, but some servers don't like @ symbols
+						return `npm.${packageName.replace('@', '')}`;
+					},
+				},
+			},
+		},
+	},
 };
 
 module.exports = [serverConfig, clientConfig];
