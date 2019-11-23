06c3552eaf059a5b61c74e9cc565dc4499beacd3
Sam Bernheim <sbernheim4@users.noreply.github.com>
2019-11-23 12:23:12 -0500

    Feat/add percentage to savings (#55)
    * Display percentage in UI
    
    * Add fr to styleint list of accepted units
    
    * Cleanup
    
    * Display percentage of total for each account
    
    * Sort rows for each institution by account balance in descending order
    
    * Fix indentation
    
    * Update color for institution name
    
    * Remove unneeded else
    
    * Update var name
    
    * Display a - instead of nothing for accounts with 0%
    
    * Sort institution cards in descending order by amount
    
    * Update var name
    
    * Cleanup
    
    * Add line break

--- src/budgeteer/Networth/NetworthTable/NetworthTable.jsx
+++ src/budgeteer/Networth/NetworthTable/NetworthTable.jsx
@@ -10,77 +10,121 @@ export default function NetworthTable(props) {
 	if (!accountBalancesLoaded) {
 
 		return (
-
 			<div className='networth--loading'>
 				<h1>Hang tight, getting your data from the cloud</h1>
 				<img src='/loading-gifs/loading-three.gif' alt='loading' />
 			</div>
-
 		);
-
-	} else {
-
-		return (
-
-			<section className='networth-table'>
-
-				{props.accountBalances.map((institution, i) => (
-					<InstitutionInfo
-						key={i}
-						displayNames={props.displayNames}
-						institutionId={institution.institutionId}
-						institutionInfo={institution.institutionBalanceObject}
-						institutionNames={props.institutionNames}
-					/>
-				))}
-
-				<div className='institution-container'>
-					<div className='networth--entry networth--total'>
-						<p className='acct-name'>Total Savings</p>
-						<p className='acct-value'>${numberWithCommas(formatAmount(props.savings))}</p>
-					</div>
-				</div>
-
-			</section>
-
-		);
-
 	}
 
+	const totalSavingsDisplay = numberWithCommas(formatAmount(props.savings));
+
+	const institutionCards = props.accountBalances.map((institution, i) => {
+		return <InstitutionInfo
+			key={i}
+			displayNames={props.displayNames}
+			institutionId={institution.institutionId}
+			institutionInfo={institution.institutionBalanceObject}
+			institutionNames={props.institutionNames}
+			totalSavings={props.savings}
+		/>
+	});
+
+	institutionCards.sort((a, b) => {
+
+		let totalOne = 0;
+		let totalTwo = 0;
+
+		for (const accountId in a.props.institutionInfo) {
+			totalOne += a.props.institutionInfo[accountId].accountBalance;
+		}
+
+		for (const accountId in b.props.institutionInfo) {
+			totalTwo += b.props.institutionInfo[accountId].accountBalance;
+		}
+
+		return totalTwo - totalOne;
+
+	});
+
+	return (
+
+		<section className='networth-table'>
+			{institutionCards}
+			<div className='institution-container'>
+				<div className='networth--entry networth--total'>
+					<p className='acct-name'>Total Savings</p>
+					<p className='acct-value'>${totalSavingsDisplay}</p>
+				</div>
+			</div>
+
+		</section>
+	);
 }
 
 function InstitutionInfo(props) {
-	function getDisplayName(acctId) {
-		return props.displayNames.get(acctId) || props.institutionInfo[acctId].accountName;
-	}
 
-	const institutionInfo = props.institutionInfo;
-	const InstitutionName = props.institutionNames[props.institutionId];
+	const {
+		displayNames,
+		institutionInfo,
+		totalSavings
+	} = props;
+
+	const institutionName = props.institutionNames[props.institutionId];
+	const institutionInfoRows = Object.keys(institutionInfo).map((acctId, i) => {
+		return <InstitutionInfoRow
+			key={i}
+			displayNames={displayNames}
+			institutionInfo={institutionInfo}
+			totalSavings={totalSavings}
+			acctId={acctId}
+		/>
+	});
+
+	institutionInfoRows.sort((a, b) => {
+		const aAccountId = a.props.acctId;
+		const bAccountId = b.props.acctId;
+		const aTotal = a.props.institutionInfo[aAccountId].accountBalance;
+		const bTotal = b.props.institutionInfo[bAccountId].accountBalance;
+
+		return bTotal - aTotal;
+	});
 
 	return (
 		<div className='institution-container'>
-			<h3>{InstitutionName}</h3>
-
-			{Object.keys(institutionInfo).map((acctId, i) => (
-
-				<div key={i} className='networth--entry'>
-					<p className='acct-name'>{getDisplayName(acctId)}</p>
-					<p className='acct-value'>
-						$
-						{numberWithCommas(
-							formatAmount(
-								institutionInfo[acctId].accountType === 'credit'
-									? institutionInfo[acctId].accountBalance * -1
-									: institutionInfo[acctId].accountBalance
-							)
-						)}
-					</p>
-				</div>
-
-			))}
-
+			<h3>{institutionName}</h3>
+			{institutionInfoRows}
 		</div>
-
 	);
+}
 
+function InstitutionInfoRow(props) {
+
+	const {
+		acctId,
+		institutionInfo,
+		totalSavings,
+		displayNames
+	} = props
+
+	function getDisplayName(acctId) {
+		return displayNames.get(acctId) || institutionInfo[acctId].accountName;
+	}
+
+	const isCreditCardAccount = institutionInfo[acctId].accountType === 'credit';
+	const accountBalance = institutionInfo[acctId].accountBalance;
+	const amountToDisplay = isCreditCardAccount ? accountBalance * -1 : accountBalance;
+
+	const accountDisplayName = getDisplayName(acctId);
+	const accountDisplayAmount = numberWithCommas(formatAmount(amountToDisplay));
+	const accountPercentage = Math.round(accountBalance / totalSavings * 100, 2);
+	const accountPercentageDisplay = accountPercentage === 0 ? <p className='acct-percentage'>-</p> : <p className='acct-percentage'>{accountPercentage}%</p>;
+
+	return (
+		<div className='networth--entry'>
+			<p className='acct-name'>{accountDisplayName}</p>
+			<p className='acct-value'>${accountDisplayAmount}</p>
+			{accountPercentageDisplay}
+		</div>
+	);
 }
