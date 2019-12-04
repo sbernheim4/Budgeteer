adc6f26a85e85f51c135a9ba307e85dfe4413184
sbernheim4 <sambernheim@gmail.com>
2019-12-03 23:50:17 -0500

    fix: show redux dev tools

--- src/budgeteer/redux/configureStore.js
+++ src/budgeteer/redux/configureStore.js
@@ -6,6 +6,7 @@ import rootReducer from './reducers';
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // shows dev tools in the console
         initialState,
         applyMiddleware(thunk)
     );
