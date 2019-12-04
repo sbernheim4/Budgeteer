2e48ab2925cb58c66e5539d9146c920275894c67
sbernheim4 <sambernheim@gmail.com>
2019-12-03 23:00:12 -0500

    fix: begin adding in redux

---
+++ src/budgeteer/redux/reducers/todosReducers.js
@@ -0,0 +1,39 @@
+function addTodo(state, action) {
+
+	const { id, text } = action;
+
+	const newTodo = {
+		id,
+		text,
+		completed: false
+	};
+
+	return [...state, newTodo];
+
+}
+
+function toggleTodo(state, action) {
+
+	return state.map(todo =>{
+		return todo.id === action.id ?
+			{...todo, completed: !todo.completed } :
+			todo
+	});
+
+}
+
+const todos = (state = [], action) => {
+
+	switch (action.type) {
+
+		case 'ADD_TODO':
+			return addTodo(state, action);
+		case 'TOGGLE_TODO':
+			return toggleTodo(state, action);
+		default:
+			return state
+	}
+}
+
+export default todos
+
