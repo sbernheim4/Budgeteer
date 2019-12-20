1e7ac117795b2fed99801f8eb3a427a191bf183a
sbernheim4 <sambernheim@gmail.com>
2019-12-20 17:09:41 -0500

    improvement: convert home component to a functional component

--- src/budgeteer/Home/Home.jsx
+++ src/budgeteer/Home/Home.jsx
@@ -1,96 +1,50 @@
-import axios from 'axios';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import Budget from './BudgetChart/BudgetChart.jsx';
 import TransactionContainer from '../AccountsContainer/TransactionContainer/TransactionContainer.jsx';
-import { dollarify } from '../helpers';
+
 import './home.scss';
 
-class Home extends Component {
-	constructor(props) {
-		super(props);
+function Home(props) {
+	let text;
 
-		this.state = {
-			total: window.sessionStorage.getItem('total') || '...',
-		};
-	}
+	if (props.loading) {
 
-	async componentDidMount() {
-		let data = await axios({
-			method: 'GET',
-			url: '/plaid-api/balance'
-		});
-
-		data = data.data;
-
-		if (data.Error) {
-			let keyAndEnv = await axios.get('/plaid-api/key-and-env');
-			keyAndEnv = keyAndEnv.data;
-
-			// Open plaid in Link Mode
-			const plaid = Plaid.create({
-				key: keyAndEnv.publicKey,
-				env: keyAndEnv.env,
-				apiVersion: 'v2',
-				clientName: 'Budgeteer',
-				product: ['transactions'],
-				token: data.publicToken,
-				onSuccess: function(public_token, metadata) {
-					console.log('Update of Account successful');
-				},
-				onExit: function(err, metadata) {
-					console.log('err:', err);
-					console.log('metadata:', metadata);
-				}
-			});
-
-			plaid.open();
-		} else {
-			const { serializedTotalSavings } = data;
-			const totalSavings = new Number(serializedTotalSavings);
-			const savings = dollarify(totalSavings);
-			this.setState({
-				total: savings
-			});
-
-			//window.sessionStorage.setItem('total', data);
-		}
-	}
-
-	render() {
-		let text;
-
-		if (this.props.loading) {
-			text = (
-				<div className='home--loading'>
-					<h1>Loading...</h1>
-					<img src='./loading-gifs/loading-one.gif' alt='loading' />
-				</div>
-			);
-		} else {
-			text = '';
-		}
-
-		return (
-			<div className='home'>
-				{text}
-
-				<h1>Your Snapshot</h1>
-
-				<div className='home--snapshot'>
-					<div className='home--snapshot--monthly-budget'>
-						<h2>Monthly Budget</h2>
-						<Budget displayInput={false} transactions={this.props.transactions} />
-					</div>
-
-					<div className='home--snapshot--transactions'>
-						<h2>Recent Transactions</h2>
-						<TransactionContainer transactions={this.props.recentTransactions} accounts={this.props.accounts} />
-					</div>
-				</div>
+		text = (
+			<div className='home--loading'>
+				<h1>Loading...</h1>
+				<img src='./loading-gifs/loading-one.gif' alt='loading' />
 			</div>
 		);
+
+	} else {
+
+		text = '';
+
 	}
+
+	return (
+
+		<div className='home'>
+			{text}
+
+			<h1>Your Snapshot</h1>
+
+			<div className='home--snapshot'>
+				<div className='home--snapshot--monthly-budget'>
+					<h2>Monthly Budget</h2>
+					<Budget displayInput={false} transactions={props.transactions} />
+				</div>
+
+				<div className='home--snapshot--transactions'>
+					<h2>Recent Transactions</h2>
+					<TransactionContainer transactions={props.recentTransactions} accounts={props.accounts} />
+				</div>
+			</div>
+		</div>
+
+    );
 }
 
 const mapStateToProps = (state) => {
@@ -107,4 +61,5 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 	return {};
 };
+
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
