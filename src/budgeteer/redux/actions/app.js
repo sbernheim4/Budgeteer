70c097a21df53222e6f2329d0396afe6de5d6cb7
sbernheim4 <sambernheim@gmail.com>
2019-12-20 18:15:18 -0500

    fix: store display names in redux

--- src/budgeteer/redux/actions/app.js
+++ src/budgeteer/redux/actions/app.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
+
 export const AppActions = {
-    STORE_TRANSACTIONS: 'transactions',
-    STORE_ACCOUNTS: 'accounts'
+	STORE_TRANSACTIONS: 'transactions',
+	STORE_ACCOUNTS: 'accounts',
+	STORE_DISPLAY_NAMES: 'displayNames'
 };
 
 export function storeTransactionsInRedux(transactions) {
@@ -18,5 +21,35 @@ export function storeAccountsInRedux(accounts) {
 		type: AppActions.STORE_ACCOUNTS,
 		payload: accounts
 	}
+
+}
+
+export function storeDisplayNames() {
+
+	return async (dispatch, getState) => {
+
+		const state = getState();
+
+		if (state.app.displayNames.length > 0) {
+			return;
+		}
+
+		const serializedDisplayNamesRequest = await axios.get('/user-info/display-names');
+		const serializedDisplayNames = serializedDisplayNamesRequest.data;
+		const displayNames = JSON.parse(serializedDisplayNames);
+		const displayNamesMap = new Map();
+
+		if (Object.keys(displayNames).length > 0) {
+			displayNames.forEach((val) => {
+				displayNamesMap.set(val[0], val[1]);
+			});
+		}
+
+		return dispatch({
+			type: AppActions.STORE_DISPLAY_NAMES,
+			payload: displayNames
+		});
+
+	};
 
 }
--- src/budgeteer/redux/reducers/app.js
+++ src/budgeteer/redux/reducers/app.js
@@ -1,26 +1,17 @@
 import { AppActions } from './../actions/app';
 
+import generalizedReducer from './generalizedReducer';
+
 export default function appReducer(state, action) {
 
 	switch (action.type) {
 		case AppActions.STORE_TRANSACTIONS:
-			return storeData(state, action, AppActions.STORE_TRANSACTIONS);
+			return generalizedReducer(state, action, AppActions.STORE_TRANSACTIONS);
 		case AppActions.STORE_ACCOUNTS:
-			return storeData(state, action, AppActions.STORE_ACCOUNTS);
+			return generalizedReducer(state, action, AppActions.STORE_ACCOUNTS);
+		case AppActions.STORE_DISPLAY_NAMES:
+			return generalizedReducer(state, action, AppActions.STORE_DISPLAY_NAMES);
 		default:
 			return state ? state : null;
 	}
-}
-
-function storeData(state, action, name) {
-
-	const { payload } = action;
-
-	const localState = {
-		[name]: payload
-	};
-
-	const newState = Object.assign({}, state, localState);
-
-	return newState;
 }
