c9d45d2b434b2cf483993b29f7f62b47fda8924a
sbernheim4 <sambernheim@gmail.com>
2020-01-07 01:33:33 -0500

    improvement: simplify redux action creator

--- src/budgeteer/redux/actions/savings.js
+++ src/budgeteer/redux/actions/savings.js
@@ -25,62 +25,20 @@ export const getSavingsChartData = (institutionId) => {
 			return;
 		}
 
-		const historicalChartData = downloadHistoricalData(institutionId);
+		const historicalSavingsData = downloadHistoricalData(institutionId);
 
-		// TODO: Merge historicalChartData with newDataPoint IFF newDataPoint has
-		// not yet been uploaded to the db
 		dispatch({
 			type: SavingsActions.STORE_SAVINGS_CHART_DATA,
 			payload: {
-				[institutionId]: historicalChartData
+				[institutionId]: historicalSavingsData
 			}
 		});
 
 		const bankInfo = state.savings.bankInfo;
 
-		await conditionallyUploadNewDataPoint(bankInfo, historicalChartData);
+		conditionallyUploadNewData(historicalSavingsData, bankInfo);
 
 	};
-
-	async function conditionallyUploadNewDataPoint(bankInfo, historicalChartData) {
-
-		const newDataPoint = bankInfo.find(val => val.institutionId === institutionId);
-
-		if (historicalChartData.length === 0) {
-			return await uploadNewDataPoint(newDataPoint);
-		}
-
-		const today = new Date();
-		const mostRecentDataPoint = historicalChartData[historicalChartData.length - 1].name;
-		const mostRecentDataPointDate = new Date(mostRecentDataPoint);
-
-		if (!isSameDay(mostRecentDataPointDate, today) && newDataPoint) {
-			return await uploadNewDataPoint(newDataPoint);
-		}
-
-	}
-
-	async function uploadNewDataPoint(newDataPoint) {
-
-		const serializedBankInfo = JSON.stringify(newDataPoint);
-
-		try {
-
-			await axios({
-				method: 'post',
-				url: '/savings/data',
-				data: serializedBankInfo,
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			});
-
-		} catch (error) {
-
-			console.log(error);
-
-		}
-	}
 
 	async function downloadHistoricalData(institutionId) {
 
@@ -113,5 +71,42 @@ export const getSavingsChartData = (institutionId) => {
 
 	}
 
-}
+	async function conditionallyUploadNewData(historicalSavingsData, bankInfo) {
 
+		if (historicalSavingsData.length === 0) {
+			return await uploadData(bankInfo);
+		}
+
+		const today = new Date();
+		const mostRecentDataPoint = historicalSavingsData[historicalSavingsData.length - 1].name;
+		const mostRecentDataPointDate = new Date(mostRecentDataPoint);
+
+		if (!isSameDay(mostRecentDataPointDate, today) && bankInfo ) {
+			return await uploadData(bankInfo);
+		}
+
+	}
+
+	async function uploadData(bankInfo) {
+
+		const serializedBankInfo = JSON.stringify(bankInfo);
+
+		try {
+
+			await axios({
+				method: 'post',
+				url: '/savings/data',
+				data: serializedBankInfo,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+		} catch (error) {
+
+			console.log(error);
+
+		}
+	}
+
+}
