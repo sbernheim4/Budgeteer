ac6b6b3ce28c9089d5f98e1a41cc67e3f30114d0
sbernheim4 <sambernheim@gmail.com>
2021-01-01 21:59:02 -0500

    feat: rewrite savings page with hooks

--- src/budgeteer/Savings/Savings.jsx
+++ src/budgeteer/Savings/Savings.jsx
@@ -1,204 +1,28 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import differenceInDays from 'date-fns/differenceInDays'
+import React from 'react';
 
-import { updateBankInfo } from './../redux/actions/savings';
-import { getDisplayNames } from './../redux/actions/app';
-
+import { useSavingsData, useDisplayNames, useInstitutionIds } from './hooks.ts';
 import SavingsTable from './SavingsTable/SavingsTable.jsx';
 
 import './savings.scss';
 
-class Savings extends Component {
+const Savings = () => {
 
-	constructor(props) {
+	useDisplayNames();
 
-		super(props);
+	const savings = useSavingsData();
+	const institutionNames = useInstitutionIds();
 
-		this.state = {
-			accountBalances: [],
-			institutionNames: [],
-			loading: true,
-			savings: 0
-		};
+	return (
 
-		this.cacheKeyPrefix = 'budgeteer--savings';
+		<div className='networth'>
+			<SavingsTable
+				savings={savings}
+				institutionNames={institutionNames}
+			/>
+		</div>
 
-		this.getInstitutionIds = this.getInstitutionIds.bind(this);
-
-	}
-
-	isLocalStorageStale() {
-
-		const serializedEntryTime = window.localStorage.getItem(`${this.cacheKeyPrefix}--entryTime`);
-		const entryTime = new Date(serializedEntryTime);
-		const now = new Date();
-		const timeElapsedInDays = differenceInDays(now, entryTime);
-		const thresholdInDays = 1;
-
-		return timeElapsedInDays > thresholdInDays;
-
-	}
-
-	async getSavingsDataFromServer() {
-
-		try {
-
-			const savingsDataRequest = await axios.get('/plaid-api/balance');
-			const savingsData = savingsDataRequest.data;
-
-			if (savingsData.Error) {
-
-				const keyAndEnv = await axios.get('/plaid-api/key-and-env');
-
-				const plaid = Plaid.create({
-					apiVersion: 'v2',
-					clientName: 'Budgeteer',
-					env: keyAndEnv.data.env,
-					product: ['balance'],
-					key: keyAndEnv.data.publicKey,
-					token: savingsData.publicToken,
-					onSuccess: function() {
-						console.log('Update of Account successful');
-					}
-				});
-
-				plaid.open();
-			}
-
-			const { totalSavings, arrayOfObjects } = savingsData;
-
-			return {
-				totalSavings,
-				bankInfo: arrayOfObjects
-			}
-
-
-		} catch (error) {
-
-			console.log(error);
-
-        }
-	}
-
-	cacheSavingsData(totalSavings, bankInfo) {
-
-		const serializedBankInfo = JSON.stringify(bankInfo);
-
-		window.localStorage.setItem(`${this.cacheKeyPrefix}--totalSavings`, totalSavings);
-		window.localStorage.setItem(`${this.cacheKeyPrefix}--arrayOfInstitutionInfo`, serializedBankInfo);
-		window.localStorage.setItem(`${this.cacheKeyPrefix}--entryTime`, new Date());
-
-	}
-
-	async componentDidMount() {
-
-		this.props.getDisplayNames();
-
-		const [
-			linkedBanks,
-			savingsData
-		] = await Promise.all([
-			this.getInstitutionIds(),
-			this.getSavingsData()
-		]);
-
-		const { savings, accountBalances, loading } = savingsData;
-
-		this.setState({
-			institutionNames: linkedBanks,
-			savings,
-			loading
-		});
-
-		this.props.updateBankInfo(accountBalances);
-
-	}
-
-	async getSavingsData() {
-
-		const serializedTotalSavings = window.localStorage.getItem(`${this.cacheKeyPrefix}--totalSavings`);
-		const serializedBankInfo = window.localStorage.getItem(`${this.cacheKeyPrefix}--arrayOfInstitutionInfo`);
-
-		if (!this.isLocalStorageStale() && serializedBankInfo !== 'undefined' && serializedTotalSavings !== 'undefined') {
-
-			const totalSavings = new Number(serializedTotalSavings);
-			const bankInfo = JSON.parse(serializedBankInfo);
-
-			return {
-				savings: totalSavings,
-				accountBalances: bankInfo,
-				loading: false
-			}
-
-		} else {
-
-			const savingsData = await this.getSavingsDataFromServer();
-			const { totalSavings, bankInfo } = savingsData;
-
-			this.cacheSavingsData(totalSavings, bankInfo);
-			this.bankInfo = bankInfo;
-
-			return {
-				savings: totalSavings,
-				accountBalances: bankInfo,
-				loading: false
-			};
-
-		}
-	}
-
-	async getInstitutionIds() {
-
-		try {
-
-			const linkedBanksRequest = await axios.get('/plaid-api/linked-accounts');
-			const linkedBanks = linkedBanksRequest.data.banks;
-
-			return linkedBanks;
-
-		} catch (error) {
-
-			return [];
-
-		}
-
-	}
-
-	render() {
-
-		return (
-
-			<div className='networth'>
-				<SavingsTable
-					savings={this.state.savings}
-					institutionNames={this.state.institutionNames}
-					storeNewChartData={this.storeNewChartData}
-				/>
-			</div>
-
-		);
-
-	}
-
-}
-
-const mapStateToProps = (state) => {
-
-	return {
-		bankInfo: state.bankInfo,
-	};
+	);
 
 };
 
-const mapDispatchToProps = (dispatch) => {
-
-	return {
-		updateBankInfo: (newBankInfo) => dispatch(updateBankInfo(newBankInfo)),
-		getDisplayNames: () => dispatch(getDisplayNames())
-	};
-
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Savings);
+export default Savings;
---
+++ src/budgeteer/Savings/hooks.ts
@@ -0,0 +1,174 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import differenceInDays from 'date-fns/differenceInDays'
+import { updateBankInfo } from './../redux/actions/savings';
+
+import { getDisplayNames } from './../redux/actions/app';
+
+export const useDisplayNames = () => {
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+
+		dispatch(getDisplayNames());
+
+	}, []);
+
+};
+
+export const useSavingsData = () => {
+
+	const dispatch = useDispatch();
+	const [savings, setSavings] = useState(0);
+
+	const cacheKeyPrefix = 'budgeteer--savings';
+
+	const cacheSavingsData = (totalSavings, bankInfo) => {
+
+		const serializedBankInfo = JSON.stringify(bankInfo);
+
+		window.localStorage.setItem(`${cacheKeyPrefix}--totalSavings`, totalSavings);
+		window.localStorage.setItem(`${cacheKeyPrefix}--arrayOfInstitutionInfo`, serializedBankInfo);
+		window.localStorage.setItem(`${cacheKeyPrefix}--entryTime`, new Date().toLocaleString());
+
+	}
+
+
+	const getSavingsDataFromServer = async () => {
+
+		try {
+
+			const savingsDataRequest = await axios.get('/plaid-api/balance');
+			const savingsData = savingsDataRequest.data;
+
+			if (savingsData.Error) {
+
+				const keyAndEnv = await axios.get('/plaid-api/key-and-env');
+
+				// @ts-ignore
+				const plaid = Plaid.create({
+					apiVersion: 'v2',
+					clientName: 'Budgeteer',
+					env: keyAndEnv.data.env,
+					product: ['balance'],
+					key: keyAndEnv.data.publicKey,
+					token: savingsData.publicToken,
+					onSuccess: function() {
+						console.log('Update of Account successful');
+					}
+				});
+
+				plaid.open();
+			}
+
+			const { totalSavings, arrayOfObjects } = savingsData;
+
+			return {
+				totalSavings,
+				bankInfo: arrayOfObjects
+			}
+
+
+		} catch (error) {
+
+			console.log(error);
+
+        }
+	}
+
+
+	const getSavingsData = async () => {
+
+		const serializedTotalSavings = window.localStorage.getItem(`${cacheKeyPrefix}--totalSavings`);
+		const serializedBankInfo = window.localStorage.getItem(`${cacheKeyPrefix}--arrayOfInstitutionInfo`);
+
+		const isLocalStorageStale = () => {
+
+			const serializedEntryTime = window.localStorage.getItem(`${cacheKeyPrefix}--entryTime`);
+			const entryTime = new Date(serializedEntryTime);
+			const now = new Date();
+			const timeElapsedInDays = differenceInDays(now, entryTime);
+			const thresholdInDays = 1;
+
+			return timeElapsedInDays > thresholdInDays;
+
+		}
+
+
+		if (!isLocalStorageStale() && serializedBankInfo !== 'undefined' && serializedTotalSavings !== 'undefined') {
+
+			const totalSavings = new Number(serializedTotalSavings);
+			const bankInfo = JSON.parse(serializedBankInfo);
+
+			return {
+				savings: totalSavings,
+				accountBalances: bankInfo
+			}
+
+		} else {
+
+			const savingsData = await getSavingsDataFromServer();
+			const { totalSavings, bankInfo } = savingsData;
+
+			cacheSavingsData(totalSavings, bankInfo);
+
+			return {
+				savings: totalSavings,
+				accountBalances: bankInfo
+			};
+
+		}
+	}
+
+	useEffect(() => {
+
+		getSavingsData().then(savingsData => {
+			const { savings, accountBalances } = savingsData;
+
+			setSavings(savings);
+
+			dispatch(updateBankInfo(accountBalances));
+
+		});
+
+	}, []);
+
+	return savings;
+
+};
+
+export const useInstitutionIds = () => {
+	const [institutionNames, setInstitutionNames] = useState([]);
+
+	const getInstitutionIds = async () => {
+
+		try {
+
+			const linkedBanksRequest = await axios.get('/plaid-api/linked-accounts');
+			const linkedBanks = linkedBanksRequest.data.banks;
+
+			return linkedBanks;
+
+		} catch (error) {
+
+			return [];
+
+		}
+
+	}
+
+	useEffect(() => {
+
+		getInstitutionIds().then(linkedBanks => {
+
+			setInstitutionNames(linkedBanks);
+
+		});
+
+	}, []);
+
+	return institutionNames;
+
+}
